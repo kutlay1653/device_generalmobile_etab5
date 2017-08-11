af0e93e8ef183d828677ee2ab4374477656d77fa
kutlay1653 <kutlaysarimehmet@gmail.com>
2017-08-11 09:39:40 +0300

    Initial commit of etab5

---
+++ omni_etab5.mk
@@ -0,0 +1,12 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Time Zone data Credits to desalesouche => http://forum.xda-developers.com/showpost.php?p=64850444&postcount=1204
+#PRODUCT_COPY_FILES += \
+#bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_MODEL="etab5" \
+    TARGET_DEVICE="etab5"
+
+PRODUCT_NAME := omni_etab5
+PRODUCT_DEVICE := etab5
